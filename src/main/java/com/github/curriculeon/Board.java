e7adfc8fc428dcbf69a75d7c26e16a0004585f60
Eblackk <aps14blacerig@gmail.com>
2022-08-22 18:42:18 -0400

    Updated Board

--- src/main/java/com/github/curriculeon/Board.java
+++ src/main/java/com/github/curriculeon/Board.java
@@ -12,6 +12,15 @@ public class Board {
 
 
     public Boolean isInFavorOfX() {
+        Character topLeft = this.matrix[0][0];
+        Character topMiddle = this.matrix[0][1];
+        Character topRight = this.matrix[0][2];
+        Character middleLeft = this.matrix[1][0];
+        Character middleMiddle = this.matrix[1][1];
+        Character middleRight = this.matrix[1][2];
+        Character bottomLeft = this.matrix[2][0];
+        Character bottomMiddle = this.matrix[2][1];
+        Character bottomRight = this.matrix[2][2];
 
         return null;
     }
