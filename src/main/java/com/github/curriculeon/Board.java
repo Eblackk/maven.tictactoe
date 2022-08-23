e5d3c7b7e47a45be13b5c5b76a298c47465db847
Eblackk <aps14blacerig@gmail.com>
2022-08-22 20:22:39 -0400

    abstracted isInFavorOf

--- src/main/java/com/github/curriculeon/Board.java
+++ src/main/java/com/github/curriculeon/Board.java
@@ -11,8 +11,7 @@ public class Board {
 
     }
 
-
-    public Boolean isInFavorOfX() {
+    public Boolean isInFavorOf(Character player) {
         Character topLeft = this.matrix[0][0];
         Character topMiddle = this.matrix[0][1];
         Character topRight = this.matrix[0][2];
@@ -25,40 +24,24 @@ public class Board {
 
         // topLeft == middleLeft && middleLeft ==  bottomLeft && bottomLeft == x
         // line 25 checks to see if leftcolumn has the same value of x
-        boolean winsWithLeftColumn = topLeft == middleLeft && middleLeft == bottomLeft && bottomLeft == 'X';
-        boolean winsWithMiddleColumn = topMiddle == middleMiddle && middleMiddle == bottomMiddle && bottomMiddle == 'X';
-        boolean winsWithRightColumn = topRight == middleRight && middleRight == bottomRight && bottomRight == 'X';
-        boolean winsWithLeftDiagonal = topLeft == middleMiddle && middleMiddle == bottomRight && bottomRight == 'X';
-        boolean winsWithRightDiagonal = topRight == middleMiddle && middleMiddle == bottomLeft && bottomLeft == 'X';
-        boolean winsWithTopRow = topLeft == topMiddle && topMiddle == topRight && topRight == 'X';
-        boolean winsWithMiddleRow = middleLeft == middleMiddle && middleMiddle == middleRight && middleRight == 'X';
-        boolean winsWithBottomRow = bottomLeft == bottomMiddle && bottomMiddle == bottomRight && bottomRight == 'X';
+        boolean winsWithLeftColumn = topLeft == middleLeft && middleLeft == bottomLeft && bottomLeft == player;
+        boolean winsWithMiddleColumn = topMiddle == middleMiddle && middleMiddle == bottomMiddle && bottomMiddle == player;
+        boolean winsWithRightColumn = topRight == middleRight && middleRight == bottomRight && bottomRight == player;
+        boolean winsWithLeftDiagonal = topLeft == middleMiddle && middleMiddle == bottomRight && bottomRight == player;
+        boolean winsWithRightDiagonal = topRight == middleMiddle && middleMiddle == bottomLeft && bottomLeft == player;
+        boolean winsWithTopRow = topLeft == topMiddle && topMiddle == topRight && topRight == player;
+        boolean winsWithMiddleRow = middleLeft == middleMiddle && middleMiddle == middleRight && middleRight == player;
+        boolean winsWithBottomRow = bottomLeft == bottomMiddle && bottomMiddle == bottomRight && bottomRight == player;
 
 
         return winsWithLeftColumn || winsWithMiddleColumn || winsWithRightColumn || winsWithLeftDiagonal || winsWithRightDiagonal || winsWithTopRow || winsWithMiddleRow || winsWithBottomRow;
     }
+    public Boolean isInFavorOfX() {
+         return isInFavorOf('X');
+    }
 
     public Boolean isInFavorOfO() {
-        Character topLeft = this.matrix[0][0];
-        Character topMiddle = this.matrix[0][1];
-        Character topRight = this.matrix[0][2];
-        Character middleLeft = this.matrix[1][0];
-        Character middleMiddle = this.matrix[1][1];
-        Character middleRight = this.matrix[1][2];
-        Character bottomLeft = this.matrix[2][0];
-        Character bottomMiddle = this.matrix[2][1];
-        Character bottomRight = this.matrix[2][2];
-
-        boolean winsWithLeftColumn = topLeft == middleLeft && middleLeft == bottomLeft && bottomLeft == 'O';
-        boolean winsWithMiddleColumn = topMiddle == middleMiddle && middleMiddle == bottomMiddle && bottomMiddle == 'O';
-        boolean winsWithRightColumn = topRight == middleRight && middleRight == bottomRight && bottomRight == 'O';
-        boolean winsWithLeftDiagonal = topLeft == middleMiddle && middleMiddle == bottomRight && bottomRight == 'O';
-        boolean winsWithRightDiagonal = topRight == middleMiddle && middleMiddle == bottomLeft && bottomLeft == 'O';
-        boolean winsWithTopRow = topLeft == topMiddle && topMiddle == topRight && topRight == 'O';
-        boolean winsWithMiddleRow = middleLeft == middleMiddle && middleMiddle == middleRight && middleRight == 'O';
-        boolean winsWithBottomRow = bottomLeft == bottomMiddle && bottomMiddle == bottomRight && bottomRight == 'O';
-
-        return winsWithLeftColumn || winsWithMiddleColumn || winsWithRightColumn || winsWithLeftDiagonal || winsWithRightDiagonal || winsWithTopRow || winsWithMiddleRow || winsWithBottomRow;
+        return isInFavorOf('O');
     }
 
     public Boolean isTie() {
